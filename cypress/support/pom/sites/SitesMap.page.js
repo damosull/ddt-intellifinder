2e271921ef0f6f83ca537cae01f8494ebc9da2f0
Prometeya <142401141+polinaprometeya@users.noreply.github.com>
2025-04-11 13:46:40 +0200

    INFI-2140-small changes to sites and category

--- cypress/support/pom/sites/SitesMap.page.js
+++ cypress/support/pom/sites/SitesMap.page.js
@@ -2,6 +2,7 @@ export class SitesMapPage {
     btnViewAllOnMapSearch = () => cy.contains('View all on map')
     pageTitle = () => cy.get('h3').contains('Sites list')
 
+    //check if lat and lon is numeric string OR 0 for 'notShowOnMap == false'
     openAndVerifySiteData() {
         cy.intercept('POST', 'api/site/sites').as('postSites');
         this.btnViewAllOnMapSearch().click();
@@ -9,9 +10,14 @@ export class SitesMapPage {
             expect(intercept.response.statusCode).to.eq(200);
             intercept.response.body.data.sites.forEach((site) => {
                 expect(site).to.have.property('name').and.be.a('string');
-                expect(site).to.have.property('lat').and.be.a('string');
-                expect(site).to.have.property('lon').and.be.a('string');
+                expect(site).to.have.property('lat').and.satisfy((lat) => {
+                    return lat === 0 || !isNaN(parseFloat(lat));
+                });
+                expect(site).to.have.property('lon').and.satisfy((lon) => {
+                    return lon === 0 || !isNaN(parseFloat(lon));
+                });
             });
-        })
+        });
     }
+    
 }
